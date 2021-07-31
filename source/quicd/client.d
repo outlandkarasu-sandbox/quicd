65ca223cec9ddfe4fb000625f004a43066e3a2e4
outland.karasu <outland.karasu@gmail.com>
2021-07-31 15:05:48 +0900

    issue #1 データ送受信まで実装した。

--- source/quicd/client.d
+++ source/quicd/client.d
@@ -3,12 +3,55 @@
 */
 module quicd.client;
 
+import std.stdio : writeln, writefln;
+
+import std.socket :
+    AddressFamily,
+    InternetAddress,
+    Socket,
+    SocketSet,
+    UdpSocket;
+
 /**
 クライアントのメイン関数
 */
 void clientMain()
 {
-    import std.stdio : writeln;
-    writeln("client");
+    auto address = new InternetAddress("127.0.0.1", 8443);
+    scope socket = new UdpSocket(AddressFamily.INET);
+
+    scope checkWrite = new SocketSet(1);
+    scope checkError = new SocketSet(1);
+
+    foreach (socketSet; [checkWrite, checkError])
+    {
+        socketSet.reset();
+        socketSet.add(socket);
+    }
+
+    immutable result = Socket.select(null, checkWrite, checkError);
+    if (result < 0)
+    {
+        writeln("interrupted.");
+        return;
+    }
+    else if (result == 0)
+    {
+        writeln("timeout");
+        return;
+    }
+
+    if (checkWrite.isSet(socket))
+    {
+        immutable data = "test";
+        immutable received = socket.sendTo(data, address);
+        if (received == Socket.ERROR)
+        {
+            writeln("error");
+            return;
+        }
+
+        writefln("sent: %s", data[0 .. received]);
+    }
 }
 
--- source/quicd/server.d
+++ source/quicd/server.d
@@ -3,12 +3,56 @@
 */
 module quicd.server;
 
+import std.stdio : writeln, writefln;
+
+import std.socket :
+    AddressFamily,
+    InternetAddress,
+    Socket,
+    SocketSet,
+    UdpSocket;
+
 /**
 サーバーのメイン関数
 */
 void serverMain()
 {
-    import std.stdio : writeln;
-    writeln("server");
+    auto address = new InternetAddress("127.0.0.1", 8443);
+    scope socket = new UdpSocket(AddressFamily.INET);
+    socket.bind(address);
+
+    scope checkRead = new SocketSet(1);
+    scope checkError = new SocketSet(1);
+
+    foreach (socketSet; [checkRead, checkError])
+    {
+        socketSet.reset();
+        socketSet.add(socket);
+    }
+
+    immutable result = Socket.select(checkRead, null, checkError);
+    if (result < 0)
+    {
+        writeln("interrupted.");
+        return;
+    }
+    else if (result == 0)
+    {
+        writeln("timeout");
+        return;
+    }
+
+    if (checkRead.isSet(socket))
+    {
+        char[1024] buffer;
+        immutable received = socket.receive(buffer);
+        if (received == Socket.ERROR)
+        {
+            writeln("error");
+            return;
+        }
+
+        writefln("received: %s", buffer[0 .. received]);
+    }
 }
 
